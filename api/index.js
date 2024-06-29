ef8d77e0d4865a68c95c05b23942888686ae9c09
NoxiousST <danielkristian268@gmail.com>
2024-06-29 22:59:10 +0700

    Act One

--- api/index.js
+++ api/index.js
@@ -3,10 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const bodyParser = require('body-parser')
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://puncakdonasi.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get("/publishkey", function (req, res) {
     res.json({publishable_key: process.env.STRIPE_PUBLISHABLE_KEY})
