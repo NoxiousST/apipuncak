7f635a6b820b90bd3313884a114190adb127ddef
NoxiousST <danielkristian268@gmail.com>
2024-06-29 21:33:16 +0700

    Act One

--- api/index.js
+++ api/index.js
@@ -2,15 +2,37 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const path = require('path');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 app.use(express.static('public'));
 
-app.get("/publishkey", function (req, res, next) {
+app.get("/publishkey", function (req, res) {
     res.json({publishable_key: process.env.STRIPE_PUBLISHABLE_KEY})
 })
 
+app.post("/create-payment-intent", async (req, res) => {
+    let { name, email, amount, note } = req.body
+    let customer
 
+    const customers = await stripe.customers.search({
+        query: `email: '${email}'`,
+    })
+
+    if (customers.data.length > 0) customer = customers.data.at(0)
+    else customer = await stripe.customers.create({ name, email })
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        customer: customer.id,
+        receipt_email: email,
+        amount,
+        metadata: {
+            "Pesan/Catatan Donatur": note,
+        },
+        currency: "idr",
+    })
+
+    return res.json({ client_secret: paymentIntent.client_secret })
+})
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
